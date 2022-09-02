37211cb66524447e8e07882d714f1ef8ba52b059
Roman Krylov <ro.krylov@x5.ru>
2022-09-02 17:09:05 +0300

    new

--- src/Main.java
+++ src/Main.java
@@ -27,6 +27,5 @@ public class Main {
                 break;
         }
         System.out.println("Бассейн наполнен за " + time + " минут");
-        System.out.println("Бассейн наполнен за " + time + " минут");
     }
 }
